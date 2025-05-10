410d397e63d9de3ff1bc2cbffd9ff272d883df19
Brenda <brendanjeri2001@gmail.com>
2025-05-10 01:28:02 -0700

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+console.log(companyName);
+const js = companyName;
+console.log(js);
+
+let mostProfitableNeighborhood= 'chelsea';
+console.log(mostProfitableNeighborhood);
+
+let companyCeo = 'Susan Smith';
+console.log(companyCeo);
+
+

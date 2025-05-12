a2480c8555f537e01b50ba1df8a01c21d96fa8b5
Brenda <brendanjeri2001@gmail.com>
2025-05-11 20:50:02 -0700

    changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file!
 let companyName = 'Scuber';
 console.log(companyName);
-const js = companyName;
-console.log(js);
+//const js = companyName;
+//console.log(js);
 
 let mostProfitableNeighborhood= 'chelsea';
 console.log(mostProfitableNeighborhood);
@@ -11,3 +11,34 @@ let companyCeo = 'Susan Smith';
 console.log(companyCeo);
 
 
+function book(pages)
+{
+    if(pages <=50){
+        answer = "short story";
+    }
+    else {
+        answer = "novel";
+    }
+    return answer;
+}
+console.log(book(50));
+
+//const table = "cities";
+let table = prompt("Enter table to check number of rows: ");
+let rows ;
+switch (table){
+    case "numbers":
+        rows = 6;
+        break;
+    case "cities":
+        rows = 2;
+        break;
+    case "age":
+        rows = 5;
+        break;
+    default :
+        console.log("invalid entry");
+        break;
+}
+
+console.log(rows);
